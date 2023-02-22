502c8721fe9a54a5403da202b057bbf46e0d5cb4
Ievgen Tovtin <zhe.tovtin@gmail.com>
2023-02-22 15:48:51 -0600

    added greeting function

--- 10_Days_challenges/1_Day_0_Hello_World-greeting-function.js
+++ 10_Days_challenges/1_Day_0_Hello_World-greeting-function.js
@@ -1,3 +1,38 @@
-const greeting = paramaterVariable => {
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function greeting (paramaterVariable) {
+  
+    // This line prints 'Hello, World!' to the console:
+    console.log('Hello, World!');
+    
+    // Write a line of code that prints parameterVariable to stdout using console.log:
     console.log(paramaterVariable);
 };
+
+function main() {
+    const parameterVariable = readLine();
+    
+    greeting(parameterVariable);
+}
