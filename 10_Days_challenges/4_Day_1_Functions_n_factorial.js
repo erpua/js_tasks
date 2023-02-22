f0c08fa3f9c13ef3279d87e102c2ec41782bf323
Ievgen Tovtin <zhe.tovtin@gmail.com>
2023-02-22 16:13:17 -0600

    added n factorial

--- 10_Days_challenges/4_Day_1_Functions_n_factorial.js
+++ 10_Days_challenges/4_Day_1_Functions_n_factorial.js
@@ -26,11 +26,41 @@ function readLine() {
  */
 
 function factorial(n) {
+  
+  // 1. Create a variable factorialResult to store n
+  let factorialResult = n;
+  
+  //2. Guard close
+  // If n = 0 OR n = 1, the factorial will return 1
+  if(n === 0 || n === 1) {
+    return 1;
+  }
+  
+  // 3. Loop all "n" factorial 
+  while(n > 1) {
+      n--; // decrementation by 1 at each iteration
+      factorialResult = factorialResult * n;
+    }
+    /* 
+                    num           num--      var result      result *= num         
+    1st iteration:   5             4            5             20 = 5 * 4      
+    2nd iteration:   4             3           20             60 = 20 * 3
+    3rd iteration:   3             2           60            120 = 60 * 2
+    4th iteration:   2             1          120            120 = 120 * 1
+    5th iteration:   1             0          120
+    End of the WHILE loop 
+    */
+  // Return the factorialResult of the provided integer 
+  return factorialResult; 
+}
+
+/* function factorial(n) {
     if (n < 2) {
         return 1;
     }
     return n * factorial(n-1);
-}
+} */
+
 
 function main() {
     const n = +(readLine());
