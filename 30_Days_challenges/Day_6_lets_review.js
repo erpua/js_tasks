c4b9d7b87688380f8058df962ae59c82877d0441
Ievgen Tovtin <zhe.tovtin@gmail.com>
2023-03-08 15:48:30 -0600

    added rectangle object

---
+++ 30_Days_challenges/Day_6_lets_review.js
@@ -0,0 +1,22 @@
+function processData(input) {
+    //Enter your code here
+    
+        const data = input.split('\n');
+        console.log('data =>', data);
+    
+       /*  for (let i = 0; i < data[0]; i++) {
+        
+          let str = data[i + 1]
+          let even = '' ;
+          let odd = '';
+        
+          for(let j = 0; j < str.length; j++){
+            (j % 2) > 0 ? odd = odd + str[j] : even = even + str[j];
+          }
+        
+          console.log(`${even} ${odd}`);
+        } */
+} 
+
+processData('Hacker');
+
